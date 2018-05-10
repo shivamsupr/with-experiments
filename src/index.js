d35881f4d5d639867c5c7624503136fad5383692
Akshay Nair <phenax5@gmail.com>
2018-05-10 14:14:02 +0530

    Adds withExperiment and experiments functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+export const experiments = (experimentConfig, userExperiments) => {
+  const { experiments: experimentMap, experimentList } = experimentConfig;
+
+  const userExperimentBuckets = {};
+
+  for (const userExperimentName of Object.keys(userExperiments)) {
+    const experiment = userExperiments[userExperimentName];
+    const experimentNameList = experimentMap[userExperimentName] || [];
+    for (const experimentName of experimentNameList) {
+      if (experimentName in experimentList) {
+        const { status, buckets } = experimentList[experimentName] || {};
+        const { bucket } = experiment || {};
+
+        if (status) {
+          userExperimentBuckets[experimentName] = {
+            status,
+            bucket: buckets[bucket] || 'A',
+          };
+        }
+      }
+    }
   }
-}
+
+  return userExperimentBuckets;
+};
+
+
+const withExperiment = (experimentName, Components, defaultBucket) => props => {
+  const { experiment } = props || {};
+  const { bucket } = experiment[experimentName] || {};
+  const Component = Components[bucket] || Components[defaultBucket];
+  return <Component {...props} />;
+};
+
+export default withExperiment;
+
