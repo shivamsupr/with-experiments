d204d9f2117744ddf4ef26bcb353b786d429a683
Akshay Nair <phenax5@gmail.com>
2018-05-10 14:48:36 +0530

    Setsup jest for testing

---
+++ tests/experiments.test.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+
+import React from 'react';
+import {render, unmountComponentAtNode} from 'react-dom';
+
+import { experiments } from '../src/';
+
+
+describe('experiments', () => {
+
+  it('should do stuff', () => {
+    expect(true).toBe(true);
+  });
+});
